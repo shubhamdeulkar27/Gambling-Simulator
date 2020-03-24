3936a0ebbdabba6af549bac61798eb80ac23dd32
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-24 10:20:54 +0530

    Added: Condition For Stake

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -4,12 +4,26 @@ stake=100
 IS_WIN=1
 IS_LOST=0
 
-bet=$(( RANDOM%2 ))
+fiftyPercent=`echo "$stake*0.5"|bc`
+lowerStakePercent=${fiftyPercent%.*}
+higherStakePercent=$(( $stake+$lowerStakePercent ))
 
+while [ true ]
+do
+bet=$(( RANDOM%2 ))
 if [ $bet -eq $IS_WIN ]
 then
 	(( stake++ ))
+	if [ $stake -eq $higherStakePercent ]
+	then
+		break
+	fi
 elif [ $bet -eq $IS_LOST ]
 then
 	(( stake-- ))
+	if [ $stake -eq $lowerStakePercent ]
+	then
+		break
+	fi
 fi
+done
