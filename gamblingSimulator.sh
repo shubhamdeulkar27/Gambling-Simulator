754b1af867d00205b1ccf9f98c874b27ef555722
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-24 09:22:24 +0530

    Added: Bet And Win and Lost Feature

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -x
 
 stake=100
-BET=1
+
+IS_WIN=1
+IS_LOST=0
+
+bet=$(( RANDOM%2 ))
+
+if [ $bet -eq $IS_WIN ]
+then
+	(( stake++ ))
+elif [ $bet -eq $IS_LOST ]
+then
+	(( stake-- ))
+fi
