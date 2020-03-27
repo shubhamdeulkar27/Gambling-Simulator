c1118b66d97a5b5e714cde0fd8a61b48044190dc
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-27 15:59:21 +0530

    Refactored: P

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -x
 
+#VARIABLES
 stake=100
 
+#CONSTANTS
 IS_WIN=1
 IS_LOST=0
 
-bet=$(( RANDOM%2 ))
-
-if [ $bet -eq $IS_WIN ]
-then
-	(( stake++ ))
-elif [ $bet -eq $IS_LOST ]
-then
-	(( stake-- ))
-fi
+function placeBet(){
+	bet=$(( RANDOM%2 ))
+	if [ $bet -eq $IS_WIN ]
+	then
+		(( stake++ ))
+	elif [ $bet -eq $IS_LOST ]
+	then
+		(( stake-- ))
+	fi
+}
